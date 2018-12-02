e59b1490b8e513e8bb9acf206d6b619c08a12970
Niloy Rahman <havocisgod@gmail.com>
2018-12-02 15:46:55 +0600

    Create abstractdemo2.java

---
+++ abstractdemo2.java
@@ -0,0 +1,23 @@
+package morejava;
+
+ class AbstractClass {
+	public static void main (String [] args) {
+	Phone p1 = new newPhone();	
+	p1.call();
+	p1.turnoff();
+	}
+
+}
+ abstract class Phone {
+	 public void call(){
+		 System.out.println("Calling....");
+		 
+	 }
+	 public abstract void turnoff();
+ }
+ class newPhone extends Phone{
+	 public void turnoff() {
+		 System.out.println("Turning off....");
+	 }
+	 
+ }
