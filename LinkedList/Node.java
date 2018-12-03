6c8939eeda88c179fa76e5dfdd2f56096eb26f64
Niloy Rahman <havocisgod@gmail.com>
2018-12-04 00:01:06 +0600

    Update Node.java

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,5 +1,31 @@
 package LinkedList;
 
 public class Node {
+	//properties
+	private Node next;
+	private int data ;
+	
+	//constructors
+	public Node(int data, Node next){
+		this.data =data;
+		this.next = next;
+	}
+	
+	public Node(int data) {
+		this.data=data;
+	}
+	//methods!
+	public Node getNext() {
+		return this.next;
+	}
+	public int getData() {
+		return this.data;
+	}
+	public void setData(int data) {
+		this.data= data;
+	}
+	public void setNext(Node next) {
+		this.next=next;
+	}
 
 }
