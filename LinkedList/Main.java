01a2e37272c229bf1a0f9966f9d11ac77828305d
Niloy Rahman <havocisgod@gmail.com>
2018-12-04 00:01:21 +0600

    Update Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,5 +1,10 @@
 package LinkedList;
 
 public class Main {
-
+ public static void main (String [] args) {
+	 Node n1 = new Node(5);
+	 LinkedListBD l1 = new LinkedListBD(n1);
+	
+	 l1.addNode(45);
+ }
 }
