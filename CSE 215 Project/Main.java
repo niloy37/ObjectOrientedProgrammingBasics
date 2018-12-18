0b6d9dd7a46f40ac2d5ffe5d4a3a813bb0c3b7a7
Niloy Rahman <havocisgod@gmail.com>
2018-12-18 23:36:06 +0600

    Create Main.java

---
+++ CSE 215 Project/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String [] args){
+        Login mf  = new Login();
+        mf.setVisible(true);
+       // mf.setSize(500,500);
+        mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
