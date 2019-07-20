ed4852aa2637a23007919c71234603346df93c1d
Niloy Rahman <havocisgod@gmail.com>
2019-07-20 20:52:53 +0600

    Create Main.java

---
+++ JavaBackRectangle/Main.java
@@ -0,0 +1,10 @@
+package RectangleBasics;
+
+public class Main {
+public static void main(String[] args) {
+	Rectangle r1 = new Rectangle(7,7);
+	System.out.println(r1.toString());
+	System.out.println(r1.getArea());
+	System.out.println(r1.getCircumferance());
+}
+}
